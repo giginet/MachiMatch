2bb0a4e772ed20a0b0ffb4ad1417f18f6136ffeb
giginet <giginet.net@gmail.com>
2011-05-12 07:18:52 +0900

    Pywaz.number改良

--- src/main/navigation.py
+++ src/main/navigation.py
@@ -5,27 +5,31 @@
 #
 
 import settings
+from pywaz.core.game import Game
 from pywaz.sprite.image import Image
 from pywaz.sprite.number import Number
 
-class Navigation(object):
+class Navigation(Image):
     NAVIGATION_POSITIONS = ((0,0), (settings.SCREENWIDTH-settings.NAVIGATION_WIDTH, 0), 
-                            (0, settings.SCREENHEIGHT-settings.NAVIGATION_HEIGHT), (settings.SCREENWIDTH-settings.NAVIGATION_WIDTH, settings.SCREENHEIGHT-settings.NAVIGATION_HEIGHT))
+                            (0, settings.SCREENHEIGHT-settings.NAVIGATION_HEIGHT), 
+                            (settings.SCREENWIDTH-settings.NAVIGATION_WIDTH, settings.SCREENHEIGHT-settings.NAVIGATION_HEIGHT)) # 各プレイヤーごとのナビゲーション表示位置
+    #　以下、各プレイヤーごとのパーツ表示位置（相対位置）
+    LABEL_POSITIONS = 0
     def __init__(self, owner):
         u"""
             owner : Playerクラスインスタンス
         """
+        super(Navigation, self).__init__(u"../resources/image/main/navigation/navigation%d.png" % owner.number)
         self.owner = owner
-        self.background = Image(u"../resources/image/main/navigation/navigation%d.png" % self.owner.number)
-        self.background.x, self.background.y = self.NAVIGATION_POSITIONS[self.owner.number]
+        self.x, self.y = self.NAVIGATION_POSITIONS[self.owner.number]
         self.label = Image(u"../resources/image/main/navigation/population.png")
-        self.label.x = self.background.x
-        self.label.y = self.background.y
+        self.label.x = self.x
+        self.label.y = self.y
         self.population = Number(u"../resources/image/main/navigation/number.png", w=18, h=45)
         self.population.x = self.label.x + 20
         self.population.y = self.label.y + 20
-    def draw(self):
-        self.background.draw()
+    def draw(self, surface=Game.get_screen()):
+        super(Navigation, self).draw()
         self.label.draw()
         self.population.draw()
     def update(self):
--- src/pywaz/sprite/number.py
+++ src/pywaz/sprite/number.py
@@ -17,12 +17,28 @@ from pywaz.sprite.image import Image
 from pywaz.core.game import Game
 
 class Number(Sprite):
-    def __init__(self, filepath, n=0, x=0, y=0, w=20, h=40, offset=0):
+    u"""
+        表示する数値を変更する : NumberInstance.n = 100
+        文字のそろえを変更する : NumberInstance.align = Number.TEXTALIGNLEFT or Number.TEXTALIGNRIGHT
+    """
+    TEXTALIGNLEFT = 0
+    TEXTALIGNRIGHT = 1
+    
+    def __init__(self, filepath, n=0, x=0, y=0, w=20, h=40, margin=0):
+        u"""
+            filepath : 文字スプライトの画像パス(0-9まで等幅で並んでいる必要がある)
+            n        : 初期表示数字
+            x,y      : 表示位置座標
+            w        : 数字一つの幅
+            h        : 画像の高さ
+            margin   : 桁同士の隙間
+        """
         super(Number, self).__init__()
         self.width = w
         self.height = h
-        self.offset = offset
+        self.margin = margin
         self.n = int(n)
+        self.align = self.TEXTALIGNLEFT # 左揃えor右揃え
         self.pre_n = self.n
         self.x = x
         self.y = y
@@ -30,15 +46,15 @@ class Number(Sprite):
         self._parse()
         
     def _parse(self):
-        place = len(str(self.n))
-        self.image = pygame.surface.Surface((self.width*place+self.offset*place, self.height))
+        place = len(str(self.n)) # 桁
+        self.image = pygame.surface.Surface((self.width*place+self.margin*place, self.height))
         self.image.set_colorkey((0,0,0))
         dest = self.image.get_rect().move(self.x, self.y)
         area = dest
         for i,s in enumerate(str(self.n)):
             n = int(s)
             image = Image(self.filepath)
-            dest = pygame.rect.Rect((self.width+self.offset)*i,0, self.width, self.height)
+            dest = pygame.rect.Rect((self.width+self.margin)*i,0, self.width, self.height)
             area = Rect(n*self.width, 0, self.width, self.height)
             self.image.blit(image.image, dest, area)
     
@@ -47,6 +63,9 @@ class Number(Sprite):
             self.pre_n = self.n
             self._parse()
         dest = self.image.get_rect().move(self.x, self.y)
+        if self.align == self.TEXTALIGNRIGHT:
+            u"""右揃えに変更する"""
+            dest = dest.move(-self.image.get_width(), 0)
         return surface.blit(self.image, dest=dest)
     
     def get_surface(self):
