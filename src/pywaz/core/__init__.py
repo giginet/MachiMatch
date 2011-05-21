3896c32d3d3738bfe96a9cb770149d88f2fb1a62
giginet <giginet.net@gmail.com>
2011-05-22 03:10:06 +0900

    フルスクリーン対応

--- src/pywaz/core/__init__.py
+++ src/pywaz/core/__init__.py
@@ -3,3 +3,4 @@ import settings
 settings.SCREENWIDTH = getattr(settings, 'SCREENWIDTH', 640)
 settings.SCREENHEIGHT = getattr(settings, 'SCREENHEIGHT', 480)
 settings.SCREENCAPTION = getattr(settings, 'SCREENCAPTION', u'Hello, Kawaz')
+settings.FULLSCREEN = getattr(settings, 'FULLSCREEN', False)
--- src/pywaz/core/window.py
+++ src/pywaz/core/window.py
@@ -4,10 +4,11 @@
 #    Created by giginet
 #
 import pygame
+import settings
 
 class Window(object):
     def __init__(self, width=640, height=480, caption=u"Hello, World"):
-        self.window = pygame.display.set_mode( (width, height) ) # 画面を作る
+        self.window = pygame.display.set_mode( (width, height), pygame.FULLSCREEN if settings.FULLSCREEN else 0) # 画面を作る
         pygame.display.set_caption(caption) # タイトル
         pygame.display.flip() # 画面を反映
         
